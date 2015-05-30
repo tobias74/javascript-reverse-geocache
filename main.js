e48934c33b84b90d0cb84961bb307612373d703f
Tobias Gassmann <tobias@gaszmann.de>
2015-05-30 15:45:28 +0200

    introduce demo-html

--- main.js
+++ main.js
@@ -4,12 +4,37 @@
 // except for 'app' ones, which are in a sibling
 // directory.
 requirejs.config({
-    baseUrl: 'lib',
+    baseUrl: 'src',
     paths: {
         app: './src'
     }
 });
 
+
+requirejs(["ReverseGeocoderCache", "TileBasedCache", "caches/SimpleCache"], function(ReverseGeocoderCache, TileBasedCache, SimpleCache){
+  var simpleCache = new SimpleCache();
+  
+  var tileBasedCache = new TileBasedCache({
+    tileSize:1000,
+    prefix:'Testing_Reverse_Geocoder_',
+    cache: simpleCache 
+  });
+  
+  var reverseGeocoderCache = new ReverseGeocoderCache({
+    tileBasedCache: tileBasedCache,
+    dataProvider: {
+      retrieveData: function(lat,lng){
+        return 'dummy-data for ' + lat + ' and ' + lng;
+      }
+    }
+  });
+  
+  alert(reverseGeocoderCache.get(10, 10));
+  alert(reverseGeocoderCache.get(10.0001, 9.9999));
+  alert(reverseGeocoderCache.get(20, 20));
+});
+
+
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs(['src/regeca-main']);
+//requirejs(['src/regeca-main']);
